c0be60a9de1597fd30f767c57bd6ad0493a76395
全卓 <zhuo.quan@outlook.com>
2026-02-13 20:31:35 +0800

    Packaging with esbuild

---
+++ esbuild.js
@@ -0,0 +1,67 @@
+const esbuild = require("esbuild");
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+const sourcemap = process.argv.includes('--sourcemap');
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+    name: 'esbuild-problem-matcher',
+
+    setup(build) {
+        build.onStart(() => {
+            console.log('[watch] build started');
+        });
+        build.onEnd((result) => {
+            result.errors.forEach(({ text, location }) => {
+                console.error(`✘ [ERROR] ${text}`);
+                console.error(`    ${location.file}:${location.line}:${location.column}:`);
+            });
+            console.log('[watch] build finished');
+        });
+    },
+};
+
+async function main() {
+    const ctx = await esbuild.context({
+        entryPoints: [
+            'src/extension.ts',
+        ],
+        bundle: true,
+        format: 'cjs',
+        minify: production,
+        sourcemap: !production || sourcemap,
+        sourcesContent: false,
+        platform: 'node',
+        outfile: 'out/extension.js',
+        external: ['vscode'],
+        logLevel: 'silent',
+        plugins: [
+            /* add to the end of plugins array */
+            esbuildProblemMatcherPlugin,
+            {
+                name: 'bson-alias',
+                setup(build) {
+                    build.onResolve({ filter: /^bson$/ }, args => {
+                        return {
+                            path: require.resolve('bson'),
+                        };
+                    });
+                },
+            },
+        ],
+    });
+    if (watch) {
+        await ctx.watch();
+    } else {
+        await ctx.rebuild();
+        await ctx.dispose();
+    }
+}
+
+main().catch(e => {
+    console.error(e);
+    process.exit(1);
+});
